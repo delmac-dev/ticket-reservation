0b44954d437d41ca5ee33752c35499c54e23bcb9
Delmac=dev <delalitengue.dev@gmail.com>
2024-03-23 12:32:44 -0400

    made all the pages for the system

--- pages/show.js
+++ pages/show.js
@@ -38,37 +38,37 @@ export function showPage(element) {
             </div>
             <div class="app_content">
                 <table class="app_content__reserve-table">
-                <thead>
-                    <tr>
-                        <th>TicketCode</th>
-                        <th>ReservationCode</th>
-                        <th>FlightCode</th>
-                        <th>Fullname</th>
-                        <th>Gender</th>
-                        <th>Class</th>
-                        <th>Seat</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>WQ8674WQ</td>
-                        <td>4898737246</td>
-                        <td>ERG4562T</td>
-                        <td>John Doe</td>
-                        <td>Female</td>
-                        <td>Economy</td>
-                        <td>R45</td>
-                    </tr>
-                    <tr>
-                        <td>OKJ342XD</td>
-                        <td>2897867533</td>
-                        <td>GJK487IO</td>
-                        <td>Jane Smith</td>
-                        <td>Female</td>
-                        <td>First Class</td>
-                        <td>K09</td>
-                    </tr>
-                </tbody>
+                    <thead>
+                        <tr>
+                            <th>TicketCode</th>
+                            <th>ReservationCode</th>
+                            <th>FlightCode</th>
+                            <th>Fullname</th>
+                            <th>Gender</th>
+                            <th>Class</th>
+                            <th>Seat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>WQ8674WQ</td>
+                            <td>4898737246</td>
+                            <td>ERG4562T</td>
+                            <td>John Doe</td>
+                            <td>Female</td>
+                            <td>Economy</td>
+                            <td>R45</td>
+                        </tr>
+                        <tr>
+                            <td>OKJ342XD</td>
+                            <td>2897867533</td>
+                            <td>GJK487IO</td>
+                            <td>Jane Smith</td>
+                            <td>Female</td>
+                            <td>First Class</td>
+                            <td>K09</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
